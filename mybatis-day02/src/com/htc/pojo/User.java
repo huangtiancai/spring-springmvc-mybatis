15a36c6652cbe107c582c032f63f2c18b17e7fd8
1812869775@qq.com <1812869775@qq.com>
2019-08-09 16:45:21 +0800

    add mybatis

---
+++ mybatis-day02/src/com/htc/pojo/User.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.htc.pojo;
+
+import java.util.Date;
+
+/**  
+* @ClassName:User
+* @Description:
+* @author:htc  
+* @date:2019年8月7日 下午6:51:16
+*/
+public class User {
+	private Integer id;
+	private String username;
+	private String sex;
+	private Date birthday;
+	private String address;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public User(Integer id, String username, String sex, Date birthday, String address) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.sex = sex;
+		this.birthday = birthday;
+		this.address = address;
+	}
+	public User() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", sex=" + sex + ", birthday=" + birthday + ", address="
+				+ address + "]";
+	}
+	
+}
